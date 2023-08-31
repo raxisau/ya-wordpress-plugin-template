931449f4a260cdbad3fc4afcbe3fc701a22b7b25
Brett (AA.ai) Dutton <brett@addaxis.ai>
2023-08-31 18:45:37 +0700

    Incremented version

--- ya-wordpress-plugin-template.php
+++ ya-wordpress-plugin-template.php
@@ -4,13 +4,13 @@
  *
  * @package       YAWPT
  * @author        Brett Dutton
- * @version       1.0.2
+ * @version       1.0.3
  *
  * @wordpress-plugin
  * Plugin Name:   Yet Another Wordpress Plugin Template
  * Plugin URI:    https://brettdutton.com
  * Description:   Shortcodes that help with any product that you see fit to run
- * Version:       1.0.2
+ * Version:       1.0.3
  * Author:        Brett Dutton
  * Author URI:    https://b2bconsultancy.asia
  * Text Domain:   ya-wordpress-plugin-template
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . '/config.php';
 
 define( 'YAWPT_NAME',        'Yet Another Wordpress Plugin Template' );
-define( 'YAWPT_VERSION',     '1.0.2' );
+define( 'YAWPT_VERSION',     '1.0.3' );
 define( 'YAWPT_PLUGIN_FILE', __FILE__ );
 define( 'YAWPT_SLUG',        basename( __FILE__, '.php' ) );
 
